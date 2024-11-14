0776b1c6267d46f4a5a3be27e1c6b81284466df1
Jordi Küffer <jordi.kueffer@uzh.ch>
2024-11-14 16:11:26 +0100

    adding direct_mis structure

---
+++ src/direct_mis.cpp
@@ -0,0 +1,30 @@
+#include <nori/warp.h>
+#include <nori/integrator.h>
+#include <nori/scene.h>
+#include <nori/emitter.h>
+#include <nori/bsdf.h>
+#include <nori/scene.h>
+
+NORI_NAMESPACE_BEGIN
+class DirectMIS : public Integrator
+{
+public:
+    DirectMIS(const PropertyList &props)
+    {
+        /* No parameters this time */
+    }
+
+    /*
+     * REFERENCES: Task description in assignment
+     */
+    Color3f Li(const Scene *scene, Sampler *sampler, const Ray3f &ray) const
+    {
+    }
+
+    std::string toString() const
+    {
+        return "Direct Multiple Importance Sampling []";
+    }
+};
+NORI_REGISTER_CLASS(DirectMIS, "direct_mis");
+NORI_NAMESPACE_END
